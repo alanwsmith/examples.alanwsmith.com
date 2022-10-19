ec2a9bf1aa42816bc362b69c0c40eeb48266f4c8
Alan Smith <commit@alanwsmith.com>
2022-10-19 13:34:56 -0400

    Added lower cased words

--- scripts/english_3000_words/make_jsons.py
+++ scripts/english_3000_words/make_jsons.py
@@ -6,14 +6,28 @@ output_dir = "../../site/english-word-lists"
 
 with open('source.txt') as _in:
     text_block = _in.read()
-    words = text_block.split("\n")
+    source_words = text_block.split("\n")
 
 with open(f"{output_dir}/3000-with-capitals.json", "w") as _out:
     data = {
         "description": "3000 of the most common English words in Capital Case where appropriate",
         "source": "https://www.ef.edu/english-resources/english-vocabulary/top-3000-words/",
+        "words": source_words,
+    }
+    json.dump(data, _out, sort_keys=True, indent=2)
+
+
+with open(f"{output_dir}/3000-lower-case.json", "w") as _out:
+    words = []
+    for word in source_words:
+        words.append(word.lower())
+    data = {
+        "description": "3000 of the most common English words lower cased",
+        "source": "https://www.ef.edu/english-resources/english-vocabulary/top-3000-words/",
         "words": words,
     }
     json.dump(data, _out, sort_keys=True, indent=2)
+
+
 
 
